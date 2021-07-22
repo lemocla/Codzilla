75a8d4649796d4bb84cd80c9b2971f91c2b2c024
lemocla <lemocla@icloud.com>
2021-07-22 13:14:29 +0000

    Add frontend validation to contact form

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
   $(document).ready(function () {
-      $('.dropdown-button').dropdown({});
+    $('.dropdown-button').dropdown({});
+
+    /*https://stackoverflow.com/questions/17048223/correct-syntax-for-this-label-jquery*/
+    $("[required]").each(function () {
+      $('label[for=' + this.id + ']').append('<span class="required"> *</span>');
+    });
   });
