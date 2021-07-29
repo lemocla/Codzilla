f82d2450e343d7a83e8b7f76c86371ca6d7a4bda
lemocla <lemocla@icloud.com>
2021-07-29 18:33:34 +0000

    Add  error page 500

--- app.py
+++ app.py
@@ -626,8 +626,12 @@ def faq():
 # https://flask.palletsprojects.com/en/1.1.x/patterns/errorpages/
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
