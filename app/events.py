1c1b8fa48cbb726a9187ca4e08235baad193f385
lemocla <lemocla@icloud.com>
2021-08-03 17:48:58 +0000

    Add details and attendees section to event page

--- app/events.py
+++ app/events.py
@@ -12,9 +12,11 @@ events = Blueprint("events", __name__)
 def event(event_id):
     event = Event.find_one_event(event_id)
     owner = User.find_user_by_id(event["created_by"])
+    attendees = list(User.find_users_by_id(event["attendees"]))
     if owner:
         admin = True
     else:
         admin = False
     return render_template("event.html", event=event,
-                           admin=admin)
+                           admin=admin, owner=owner,
+                           attendees=attendees)
