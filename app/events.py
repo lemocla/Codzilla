62152b73fba3610d13c04476df6419051e2a6faa
lemocla <lemocla@icloud.com>
2021-08-03 13:17:58 +0000

    Add header section to event page

--- app/events.py
+++ app/events.py
@@ -2,7 +2,7 @@ from flask import (flash, render_template, redirect,
                    request, url_for, session, Blueprint,
                    jsonify)
 from app.models.event import Event
-
+from app.models.user import User
 
 # Blueprint
 events = Blueprint("events", __name__)
@@ -11,4 +11,10 @@ events = Blueprint("events", __name__)
 @events.route("/event/<event_id>")
 def event(event_id):
     event = Event.find_one_event(event_id)
-    return render_template("event.html", event=event)
+    owner = User.find_user_by_id(event["created_by"])
+    if owner:
+        admin = True
+    else:
+        admin = False
+    return render_template("event.html", event=event,
+                           admin=admin)
