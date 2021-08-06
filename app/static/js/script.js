d02e7551c1b5a7ef73b1e3e1b6ca7b828833c466
lemocla <lemocla@icloud.com>
2021-08-06 17:49:00 +0000

    Display map on event page and refactor google search

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -194,24 +194,26 @@
       }
     });
 
-
     //Search address
-    $(function () {
+    $('#event_location').focusin(function () {
       const input = document.getElementById("event_location");
       const option = {
-        fields:["formatted_address", "name", "url"]
+        fields: ["formatted_address", "name", "geometry"]
       }
-      const searchBox = new google.maps.places.SearchBox(input);
-      
-      searchBox.addListener("places_changed", function(){
-      
-      const places = searchBox.getPlaces();
-      const place = places["0"];
-      //
-      let url = document.getElementById("map_url");
-      input.value = place.name + ", " + place.formatted_address;
-      url.value = place.url;
+      const searchBox = new google.maps.places.SearchBox(input, option);
+
+      searchBox.addListener("places_changed", function () {
+
+        const places = searchBox.getPlaces();
+        const place = places["0"];
+        //
+        if (place.types[0] == "premise" || place.types[0] == "street_address")
+          input.value = place.formatted_address;
+        else {
+          input.value = place.name + ", " + place.formatted_address;
+        }
       })
     });
+
 
   });
