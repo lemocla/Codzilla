5fb79198cd9f6a2036ff65d4b24af34a68eae43d
lemocla <lemocla@icloud.com>
2021-08-14 22:42:43 +0000

    Add validation for location fields

--- app/static/js/googlemaps.js
+++ app/static/js/googlemaps.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
             fields: ["formatted_address", "name", "geometry"]
         }
         const searchBox = new google.maps.places.SearchBox(input, option);
+        input.setAttribute("data-place", "search");
 
         searchBox.addListener("places_changed", function () {
 
@@ -14,13 +15,32 @@ $(document).ready(function () {
             const place = places["0"];
             //
             input.setAttribute("data-place", "selected");
-            if (place.types[0] == "premise" || place.types[0] == "street_address")
+            if (place.types[0] == "premise" || place.types[0] == "street_address") {
                 input.value = place.formatted_address;
-            else {
+                input.setAttribute("data-place", "selected");
+                $(`p[data-error=event_location]`).addClass("hide").html("");
+
+            } else {
                 input.value = place.name + ", " + place.formatted_address;
+                input.setAttribute("data-place", "selected");
+                $(`p[data-error=event_location]`).addClass("hide").html("");
             }
+            //
         })
-        
+    });
+
+    //controls pac-target-input
+    $('#event_location').change(function () {
+        console.log("focus out happening")
+        if ($(this).attr("data-place") == "search") {
+            let errorMsg = "Please select a valid address";
+            // display message
+            $(`p[data-error=event_location]`).html(errorMsg).removeClass("hide");
+        }
+        if ($(this).attr("data-place") == "selected") {
+            // hide field if valid
+            $(`p[data-error=event_location]`).addClass("hide").html("");
+        }
     });
 
 });
