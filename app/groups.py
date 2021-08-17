080fee6f2a830019d8ed18bd8e8093509257ba6f
lemocla <lemocla@icloud.com>
2021-08-17 16:38:58 +0000

    Fix delete group functionality and add height to notification

--- app/groups.py
+++ app/groups.py
@@ -1,6 +1,5 @@
 from flask import (flash, render_template, redirect,
-                   request, url_for, session, Blueprint,
-                   jsonify)
+                   request, url_for, session, Blueprint)
 from app.models.group import Group
 from app.models.event import Event
 from app.models.user import User
@@ -13,6 +12,10 @@ groups = Blueprint("groups", __name__)
 
 @groups.route("/group/<group_id>")
 def group(group_id):
+    """
+    Render group page
+    Pass variable to display relevant user actions
+    """
     group = Group.find_one_group(group_id)
     events = list(Event.find_events_by_id(group["events"]))
     users = list(User.find_users_by_id(group["group_members"]))
@@ -53,6 +56,14 @@ def group(group_id):
 
 @groups.route("/add_group", methods=["GET", "POST"])
 def add_group():
+    """
+    Check if user in session
+    If user not in session redirect to login
+    Render add a group page
+    Prepare the dictionary and insert in Mongo DB
+    Flash message to inform user of group successfully added
+    Redirect to my groups
+    """
     if not session["email"]:
         return redirect(url_for('login'))
     user = User.check_existing_user(session["email"])
@@ -84,7 +95,16 @@ def add_group():
 
 @groups.route("/edit_group/<group_id>", methods=["GET", "POST"])
 def edit_group(group_id):
-
+    """
+    Render edit group page
+    Check if user in session, if not redirect to login
+    If user is group admin, prepare dictionary for insert
+    Check if url image is valid,
+    If valid, update group and redirect to group page
+    Flash message to inform user group successfully updated
+    If not valid, return to edit group page
+    Flash message to infom user image url is invalid
+    """
     if not session["email"]:
         return redirect(url_for('login'))
 
@@ -103,6 +123,16 @@ def edit_group(group_id):
                          "img_url": request.form.get("img_url")}
             check = validators.check_img_url(request.form.get("img_url"))
             if check:
+                # Update events if group name changes
+                if group["group_name"] != request.form.get(
+                   "group_name").lower():
+                    events = list(Event.find_events_by_id(group["events"]))
+                    if len(events) > 0:
+                        for event in events:
+                            info = {"group": request.form.get(
+                                    "group_name").lower()}
+                            Event.update_event(event["_id"], info)
+                # Update group
                 Group.update_group(group_id, edit_info)
                 flash("Group successfully edited!")
                 return redirect(url_for('users.my_groups'))
@@ -118,16 +148,32 @@ def edit_group(group_id):
 
 
 # delete group modals
-@groups.route('/delete-group-modal/<group_id>', methods=['GET', 'POST'])
-def delete_group_modal(group_id):
-    group = Group.find_one_group(group_id)
-    message = group["group_name"]
-    return jsonify(message)
-
-
-# delete group modals
 @groups.route('/delete-group/<group_id>', methods=['GET', 'POST'])
 def delete_group(group_id):
+    """
+    If user not in session redirect to login
+    If user in session
+    Remove from user following
+    Remove from user owning
+    Remove group name from event
+    Delete group from MongoDB
+    Flash message informing user group succesfully deleted
+    """
+    if not session["email"]:
+        return redirect(url_for('login'))
+
+    user = User.check_existing_user(session["email"])
+    group = Group.find_one_group(group_id)
+    # Remove from users group following and group owned
+    User.remove_from_list(user["_id"], "group_following", group_id)
+    User.remove_from_list(user["_id"], "group_owned", group_id)
+    # Remove group from related events
+    events = list(Event.find_events_by_id(group["events"]))
+    if len(events) > 0:
+        for event in events:
+            info = {"group": None}
+            Event.update_event(event["_id"], info)
     Group.delete_one_group(group_id)
+
     flash("Your group has been successfully deleted")
     return redirect(url_for('users.my_groups'))
